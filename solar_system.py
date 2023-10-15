83758abea4fce6a0d9c70339d0e5f48d16564650
Florian Huber <florian.huber@hs-duesseldorf.de>
2023-10-15 21:35:57 +0200

    make it more sparkling

--- solar_system.py
+++ solar_system.py
@@ -5,20 +5,20 @@ import random
 from planets import mars
 
 
-def sparkle():
+def sparkle(width: int = 60, num_sparkle_lines: int = 2):
     # Define a set of sparkling characters.
-    sparkling_chars = '*+.o:°~ '
+    sparkling_chars = "*+.°^><o°~\'" + 100 * " "  # more spaces means more cosmic emptyness
     
-    # Generate a string of 40 random sparkling characters.
-    line = "".join(random.choice(sparkling_chars) for _ in range(40))
-    
-    print(line)
+    for _ in range(num_sparkle_lines):
+        # Generate a string of `width` random sparkling characters.
+        line = "".join(random.choice(sparkling_chars) for _ in range(width))
+        print(line)
 
 
 if __name__ == "__main__":
-    sparkle()
-    print(">>>>  Welcome to our solar system  <<<<")
-    sparkle()
-    print(f"\n{mars.NAME}:")
+    sparkle(num_sparkle_lines=3)
+    print(f"{8 * '<>'} Welcome to our solar system {8 * '<>'}")
+    sparkle(num_sparkle_lines=3)
+    print(f"{mars.NAME.upper()}:")
     print(mars.DESCRIPTION)
     sparkle()
